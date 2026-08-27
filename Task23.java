be66260b630ea879442798dd3c3b0a67dffae1e6
GOWTHAM <vtu29353@veltech.edu.in>
2026-08-27 11:57:09 +0530

    Delete Task23.java

--- Task23.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class Task23 {
-    public static void main(String[] args) {
-        LocalDate date = LocalDate.of(2024, 12, 31);
-        System.out.println(date.getDayOfYear());
-    }
-}
